9b052322b6b1862969267327a085772eb0b5cbdd
elcoxo <38883294+elcoxo@users.noreply.github.com>
2025-01-26 16:00:25 +0300

    feat: database connection

---
+++ vote-service/database.py
@@ -0,0 +1,27 @@
+import os
+from dotenv import load_dotenv
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+load_dotenv() 
+
+DB_HOST=os.getenv('DB_HOST')
+DB_DATABASE=os.getenv('DB_DATABASE')
+DB_USERNAME=os.getenv('DB_USERNAME')
+DB_PASSWORD=os.getenv('DB_PASSWORD')
+
+SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}/{DB_DATABASE}"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
