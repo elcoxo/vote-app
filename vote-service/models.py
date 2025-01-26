f92491bdaa36815b78d840ef06fc7e69c9c0b0f1
elcoxo <38883294+elcoxo@users.noreply.github.com>
2025-01-26 16:07:38 +0300

    feat: user model added

---
+++ vote-service/models.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, Text
+from sqlalchemy.orm import relationship
+from datetime import datetime
+from database import Base
+
+class User(Base):
+    __tablename__ = "users"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String(50), unique=True, index=True)
+    email = Column(String(100), unique=True, index=True)
+    hashed_password = Column(String(100))
+    
+    polls = relationship("Poll", back_populates="owner")
+    votes = relationship("UserOption", back_populates="user")
+    posts = relationship("Post", back_populates="author")
