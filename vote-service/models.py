52f0f616b9939ddc46966962db1d312a2c2549eb
elcoxo <38883294+elcoxo@users.noreply.github.com>
2025-01-26 16:09:22 +0300

    feat: poll and poll option model added

--- vote-service/models.py
+++ vote-service/models.py
@@ -14,3 +14,36 @@ class User(Base):
     polls = relationship("Poll", back_populates="owner")
     votes = relationship("UserOption", back_populates="user")
     posts = relationship("Post", back_populates="author")
+
+class Poll(Base):
+    __tablename__ = "polls"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String(100))
+    description = Column(Text)
+    owner_id = Column(Integer, ForeignKey("users.id"))
+    
+    owner = relationship("User", back_populates="polls")
+    options = relationship("Option", back_populates="poll", cascade="all, delete-orphan")
+    posts = relationship("Post", back_populates="poll")
+
+class Option(Base):
+    __tablename__ = "options"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    text = Column(String(100))
+    poll_id = Column(Integer, ForeignKey("polls.id"))
+    
+    poll = relationship("Poll", back_populates="options")
+    votes = relationship("UserOption", back_populates="option")
+
+class UserOption(Base):
+    __tablename__ = "user_options"
+    
+    user_id = Column(Integer, ForeignKey("users.id"), primary_key=True)
+    option_id = Column(Integer, ForeignKey("options.id"), primary_key=True)
+    poll_id = Column(Integer, ForeignKey("polls.id"))  # Добавили poll_id
+    
+    user = relationship("User", back_populates="votes")
+    option = relationship("Option", back_populates="votes")
+
